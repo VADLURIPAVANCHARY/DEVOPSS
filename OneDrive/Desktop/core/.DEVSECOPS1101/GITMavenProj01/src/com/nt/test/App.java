54503174744194b20ebb011e5f6ca863f5067c0d
VADLURIPAVANCHARY <152369009+VADLURIPAVANCHARY@users.noreply.github.com>
2024-09-04 20:36:59 +0530

    Delete OneDrive/Desktop/core/.DEVSECOPS1101/GITMavenProj01 directory

--- OneDrive/Desktop/core/.DEVSECOPS1101/GITMavenProj01/src/com/nt/test/App.java
+++
@@ -1,9 +0,0 @@
-package com.nt.test;
-
-public class App {
-public static void main(String[] args) {
-System.out.println("chary");
-}
-}
-
-
--- OneDrive/Desktop/core/.DEVSECOPS1101/GITMavenProj01/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module GITMavenProj01 {
-}
